41a9a42755ffe25903e42a9f3359339d9196a420
Yash Verma <yashvrm3107@gmail.com>
2025-02-26 21:27:31 +0530

    first commit-initalized all the pages till now and added doctors data in assests folder

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import MyAppointment from './pages/MyAppointment'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/doctors' element={<Doctors></Doctors>}></Route>
+        <Route path='/doctors/:speciality' element={<Doctors></Doctors>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/my-profile' element={<MyProfile></MyProfile>}></Route>
+        <Route path='/my-appointment' element={<MyAppointment></MyAppointment>}></Route>
+        <Route path='/appointment/:docId' element={<Appointment></Appointment>}></Route>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
