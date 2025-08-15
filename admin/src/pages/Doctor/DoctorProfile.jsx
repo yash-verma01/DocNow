d30f4c1eacda8246995a60049c51a7253a6e039e
Yash Verma <yashvrm3107@gmail.com>
2025-08-16 05:17:34 +0530

    doctor profile started

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useContext } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { DoctorContext } from '../../context/DoctorContext';
 
 const DoctorProfile = () => {
+    const{dtoken,profileData,setProfileData,getProfileData,updateProfileData  } = useContext(DoctorContext);
+    const {currency}=useContext(AppContext);
   return (
-    <div>
+    <div>   
       
     </div>
   )
